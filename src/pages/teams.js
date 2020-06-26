1093f93ba6059c815fc8d92dd74b0bfea4ad80f5
Harsh Rai <harshpayal3252@gmail.com>
2020-06-27 00:21:54 +0530

    navbar for blog

---
+++ src/pages/teams.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TeamsPage = () => (
+  <Layout>
+    <SEO title="Teamspage" />
+    <h1>Teams</h1>
+  </Layout>
+)
+
+export default TeamsPage
