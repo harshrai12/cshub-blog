23cafa4f0685839bf4d7d88343eafe0a4a4c99a0
Harsh Rai <harshpayal3252@gmail.com>
2020-12-08 18:20:22 +0530

    Todo App

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,66 +1,54 @@
+
 import React from "react"
-import { graphql, StaticQuery } from "gatsby"
-import { Row, Col } from "reactstrap"
-
 import Layout from "../components/layout"
-
 import SEO from "../components/seo"
+import Header from "../components/header"
+import discord from"../images/kisspng-discord-computer-icons-logo-online-chat-discord-5b2291673db518.4803332115289921032528.png"
 
-import Post from "../components/Post"
-import Sidebar from "../components/sidebar"
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Recent Articles</h1>
-    <Row>
-      <Col md="8">
-        <StaticQuery
-          query={indexQuery}
-          render={data => {
-            return (
-              <div>
-                {data.allContentfulCshub.edges.map(({ node }) => (
-                  <Post
-                    title={node.title}
-                    path={node.slug}
-                    author={node.author}
-                    date={node.date}
-                    fluid={node.images.fluid}
-                  />
-                ))}
-              </div>
-            )
-          }}
-        />
-      </Col>
-      <Col md="4">
-        <div>
-          <Sidebar />
-        </div>
-      </Col>
-    </Row>
-  </Layout>
+import logo from "../images/CSCODERSHUB3.png"
+import BackgroundImage from "gatsby-background-image"
+import { graphql } from "gatsby"
+import Team from "../components/teammember"
+
+const Home = props => (
+  <div>
+    <Header />
+    <SEO title="About" />
+   
+    {/** 
+ <div className="front">
+      <img src={logo} className="front" alt="dp" />
+    </div> 
+**/}
+    <p class="line anim-typewriter">CSCODERSHUB</p>
+    <div className="text">
+      <p>Community of coders and hackers</p>
+      <a href= "https://discord.com/invite/K5K9Yaz" className="button1">
+      <img src={discord} className="dlogo"/>Connect to Discord 
+      </a>
+    </div>
+
+    <div className="abouts-section">
+      <h1>About CSCODERSHUB</h1>
+      <p className="about-text">
+      CSCODERSHUB is a community of coders and cyber security enthusiasts, here we believe in sharing/discussing our knowledge with other members for enhancement and learning. We provide free guidance to beginners of the community. CSCODERSHUB team consists of hardworking and focused members who are ready to serve for the community without any personal benefit. We are dedicated towards building a community which can provide opportunity to members (beginners) to learn more about new technologies for free of cost.
+      Anyone can Join Us as a member or as a team. for more details contact cscodershub@gmail.com or admin@cscodershub.tech
+      </p>
+      <Team/>
+    </div>
+  </div>
 )
 
-const indexQuery = graphql`
+export default Home
+
+export const pageQuery = graphql`
   query {
-    allContentfulCshub ( sort: { fields: date, order: DESC } ) {
-      edges {
-        node {
-          title
-          slug
-          path
-          date
-          author
-          images {
-            title
-            fluid(maxWidth: 600, maxHeight:300) {
-              ...GatsbyContentfulFluid
-            }
-          }
+    indexImage: file(relativePath: { eq: "bg.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 3000) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
   }
 `
-export default IndexPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,67 @@
+
 import React from "react"
+import { graphql, StaticQuery } from "gatsby"
+import { Row, Col } from "reactstrap"
+
 import Layout from "../components/layout"
+
 import SEO from "../components/seo"
-import Header from "../components/header"
-import discord from"../images/kisspng-discord-computer-icons-logo-online-chat-discord-5b2291673db518.4803332115289921032528.png"
 
-import logo from "../images/CSCODERSHUB3.png"
-import BackgroundImage from "gatsby-background-image"
-import { graphql } from "gatsby"
-import Team from "../components/teammember"
-
-const Home = props => (
-  <div>
-    <Header />
-    <SEO title="About" />
-   
-    {/** 
- <div className="front">
-      <img src={logo} className="front" alt="dp" />
-    </div> 
-**/}
-    <p class="line anim-typewriter">CSCODERSHUB</p>
-    <div className="text">
-      <p>Community of coders and hackers</p>
-      <a href= "https://discord.com/invite/K5K9Yaz" className="button1">
-      <img src={discord} className="dlogo"/>Connect to Discord 
-      </a>
-    </div>
-
-    <div className="abouts-section">
-      <h1>About CSCODERSHUB</h1>
-      <p className="about-text">
-        is simply dummy text of the printing and typesetting industry. Lorem
-        Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a
-        type specimen book. It has survived not only five centuries, but also
-        the leap into electronic typesetting, remaining essentially unchanged.
-        It was popularised in the 1960s with the release of Letraset sheets
-        containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
-      </p>
-      <Team/>
-    </div>
-  </div>
+import Post from "../components/Post"
+import Sidebar from "../components/sidebar"
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Recent Articles</h1>
+    <Row>
+      <Col md="8">
+        <StaticQuery
+          query={indexQuery}
+          render={data => {
+            return (
+              <div>
+                {data.allContentfulCshub.edges.map(({ node }) => (
+                  <Post
+                    title={node.title}
+                    path={node.slug}
+                    author={node.author}
+                    date={node.date}
+                    fluid={node.images.fluid}
+                  />
+                ))}
+              </div>
+            )
+          }}
+        />
+      </Col>
+      <Col md="4">
+        <div>
+          <Sidebar />
+        </div>
+      </Col>
+    </Row>
+  </Layout>
 )
 
-export default Home
-
-export const pageQuery = graphql`
+const indexQuery = graphql`
   query {
-    indexImage: file(relativePath: { eq: "bg.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 3000) {
-          ...GatsbyImageSharpFluid
+    allContentfulCshub ( sort: { fields: date, order: DESC } ) {
+      edges {
+        node {
+          title
+          slug
+          path
+          date
+          author
+          images {
+            title
+            fluid(maxWidth: 600, maxHeight:300) {
+              ...GatsbyContentfulFluid
+            }
+          }
         }
       }
     }
   }
 `
+export default IndexPage
